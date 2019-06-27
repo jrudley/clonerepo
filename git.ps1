4025148c26c75f6b09ecc93be32149dac341e2bb
James Rudley <jrudley@users.noreply.github.com>
2019-06-27 16:51:13 -0500

    Update git.ps1

--- git.ps1
+++ git.ps1
@@ -33,19 +33,19 @@ git clone https://github.com/jrudley/Sitecore.HabitatHome.Platform.git
 $dac64 = "https://download.microsoft.com/download/5/E/4/5E4FCC45-4D26-4CBE-8E2D-79DB86A85F09/EN/x64/DacFramework.msi"
 $dac86 = "https://download.microsoft.com/download/5/E/4/5E4FCC45-4D26-4CBE-8E2D-79DB86A85F09/EN/x86/DacFramework.msi"
 
+            Write-Verbose "Downloading DACFx x86 from $dac86..."
+            Invoke-WebRequest -Uri $dac86 -OutFile c:\temp\DacFramework2016-x86.msi -Verbose:$VerbosePreference
+            Write-Verbose "Download of DACFx x86 successful."
+            Write-Verbose "Installing DACFx x86..."
+            Start-Process msiexec.exe -NoNewWindow -Wait -ArgumentList "/i c:\temp\DacFramework2016-x86.msi /quiet /qn /norestart"
+            Write-Verbose "Installation of DACFx x86 successful."
+            
             Write-Verbose "Downloading DACFx x64 from $dac64..."
             Invoke-WebRequest -Uri $dac64 -OutFile c:\temp\DacFramework2016-x64.msi -Verbose:$VerbosePreference
             Write-Verbose "Download of DACFx x64 successful."
             Write-Verbose "Installing DACFx x64..."
             Start-Process msiexec.exe -NoNewWindow -Wait -ArgumentList "/i c:\temp\DacFramework2016-x64.msi /quiet /qn /norestart"
             Write-Verbose "Installation of DACFx x64 successful."
-
-            Write-Verbose "Downloading DACFx x86 from $dac86..."
-            Invoke-WebRequest -Uri $dac86 -OutFile c:\temp\DacFramework2016-x86.msi -Verbose:$VerbosePreference
-            Write-Verbose "Download of DACFx x86 successful."
-            Write-Verbose "Installing DACFx x86..."
-            Start-Process msiexec.exe -NoNewWindow -Wait -ArgumentList "/i c:\tempDacFramework2016-x86.msi /quiet /qn /norestart"
-            Write-Verbose "Installation of DACFx x86 successful."
 
 
 $cakeconfig = get-content C:\projects\Sitecore.HabitatHome.Platform\cake-config.json | convertfrom-json
