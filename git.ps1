638061479b227eeed042d72953b4a8b55a3478cf
James Rudley <jrudley@users.noreply.github.com>
2019-06-27 08:35:14 -0500

    Create git.ps1

---
+++ git.ps1
@@ -0,0 +1,15 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+New-Item -Path c:\temp -ItemType Directory
+invoke-restmethod -uri 'https://github.com/git-for-windows/git/releases/download/v2.22.0.windows.1/Git-2.22.0-64-bit.exe' -OutFile c:\temp\git.exe
+
+$msbuild = "C:\temp\git.exe"
+$arguments = '/silent /suppressmsgboxes /log="c:\temp\gitinstall.log"'
+start-process $msbuild $arguments 
+
+New-Item c:\projects -ItemType directory
+Set-Location c:\projects
+git clone https://github.com/Sitecore/Sitecore.HabitatHome.Platform.git
+
+
+
+
