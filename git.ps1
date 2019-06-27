02ab9d96fd9ac2ef027c6ff9268d214ef66c03cf
James Rudley <jrudley@users.noreply.github.com>
2019-06-27 16:56:59 -0500

    Update git.ps1

--- git.ps1
+++ git.ps1
@@ -14,6 +14,8 @@ param (
 New-Item -Path c:\temp -ItemType Directory
 invoke-restmethod -uri 'https://www.7-zip.org/a/7z1900-x64.msi' -outfile 'c:\temp\7zip.msi'
 invoke-restmethod -uri 'https://github.com/git-for-windows/git/releases/download/v2.22.0.windows.1/Git-2.22.0-64-bit.exe' -OutFile c:\temp\git.exe
+invoke-restmethod -uri 'https://download.microsoft.com/download/5/E/4/5E4FCC45-4D26-4CBE-8E2D-79DB86A85F09/EN/x64/DacFramework.msi' -outfile c:\temp\DacFramework2016-x64.msi
+invoke-restmethod -uri 'https://download.microsoft.com/download/5/E/4/5E4FCC45-4D26-4CBE-8E2D-79DB86A85F09/EN/x86/DacFramework.msi' -outfile c:\temp\DacFramework2016-x86.msi
 
 Start-Process -FilePath "msiexec" -ArgumentList "/i c:\temp\7zip.msi /quiet /norestart" -Wait
 
@@ -30,22 +32,9 @@ git clone https://github.com/jrudley/Sitecore.HabitatHome.Platform.git
 
 
 
-$dac64 = "https://download.microsoft.com/download/5/E/4/5E4FCC45-4D26-4CBE-8E2D-79DB86A85F09/EN/x64/DacFramework.msi"
-$dac86 = "https://download.microsoft.com/download/5/E/4/5E4FCC45-4D26-4CBE-8E2D-79DB86A85F09/EN/x86/DacFramework.msi"
-
-Write-Verbose "Downloading DACFx x86 from $dac86..."
-Invoke-WebRequest -Uri $dac86 -OutFile c:\temp\DacFramework2016-x86.msi -Verbose:$VerbosePreference
-Write-Verbose "Download of DACFx x86 successful."
-Write-Verbose "Installing DACFx x86..."
-Start-Process msiexec.exe -NoNewWindow -Wait -ArgumentList "/i c:\temp\DacFramework2016-x86.msi /quiet /qn /norestart /L*V c:\temp\dac86.log"
-Write-Verbose "Installation of DACFx x86 successful."
-            
-Write-Verbose "Downloading DACFx x64 from $dac64..."
-Invoke-WebRequest -Uri $dac64 -OutFile c:\temp\DacFramework2016-x64.msi -Verbose:$VerbosePreference
-Write-Verbose "Download of DACFx x64 successful."
-Write-Verbose "Installing DACFx x64..."
+Start-Process msiexec.exe -NoNewWindow -Wait -ArgumentList "/i c:\temp\DacFramework2016-x86.msi /quiet /qn /norestart /L*V c:\temp\dac86.log" 
 Start-Process msiexec.exe -NoNewWindow -Wait -ArgumentList "/i c:\temp\DacFramework2016-x64.msi /quiet /qn /norestart /L*V c:\temp\dac64.log"
-Write-Verbose "Installation of DACFx x64 successful."
+
 
 
 $cakeconfig = get-content C:\projects\Sitecore.HabitatHome.Platform\cake-config.json | convertfrom-json
